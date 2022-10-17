7ba4172d804a6531aaf9cc625cb8b4466166de1b
alijonoff <2110040@newuzbekistanuniversity.uz>
2022-10-17 18:15:41 +0500

    jhk

--- src/main/java/lang/print/gaps/task6/IntegerTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/IntegerTypeCasting.java
@@ -2,5 +2,10 @@ package lang.print.gaps.task6;
 
 public class IntegerTypeCasting {
     public static void main(String[] args) {
+        int first = 100;
+        int second = first;
+        System.out.println(first);
+        System.out.println(second);
+
     }
 }
